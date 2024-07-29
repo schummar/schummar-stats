b5f2fd196e91cb0346eaa80fa89c7db21859af87
Marco Schumacher <marco@schumacher.dev>
2024-07-29 09:11:10 +0200

    fix: some updates

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { ChildProcess, exec } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 import task from 'tasuku';
+import { red } from 'kolorist';
 
 const args = process.argv.slice(2);
 
@@ -8,24 +9,36 @@ if (args[0]?.match(/^-n=\d+$/)) {
   n = Number(args[0].replace('-n=', ''));
   args.splice(0, 1);
 }
-const command = args.join(' ');
+const name = args.join(' ');
+const command = [...args];
 
 const fTime = (t: number) => `${(t / 1000).toFixed(3)}s`;
 
 task('', async ({ setTitle, setStatus, setOutput }) => {
-  setStatus(command);
+  setStatus(name);
 
   let i = 0,
     start = performance.now(),
     times = 0,
     succ = 0,
     fail = 0;
+  const errors = new Set<string>();
 
   const update = () => {
     setTitle([`Running iteration ${i + 1}`, n === Infinity ? '' : `/${n}`, ', ', fTime(performance.now() - start)].join(''));
 
+    const output = [`Successful: ${Math.round((succ / i) * 100)}%`, ` (${succ}:${fail})`, `, `, `average time: ${fTime(times / i)}`];
+
+    for (const error of errors) {
+      let oneliner = error.trim().replace(/\n/g, ' ');
+      if (oneliner.length > 30) {
+        oneliner = oneliner.slice(0, 30) + '...';
+      }
+      output.push(red(`\n  error: ${oneliner}`));
+    }
+
     if (i > 0) {
-      setOutput([`Successful: ${Math.round((succ / i) * 100)}%`, ` (${succ}:${fail})`, `, `, `average time: ${fTime(times / i)}`].join(''));
+      setOutput(output.join(''));
     }
   };
   update();
@@ -45,21 +58,41 @@ task('', async ({ setTitle, setStatus, setOutput }) => {
       childProcess?.kill('SIGKILL');
       killed = true;
       setTimeout(() => process.exit(), 1000).unref();
-    })
+    }),
   );
+
+  console.log(process.env.SHELL);
 
   for (; i < n; i++) {
     start = performance.now();
+    let combinedOutput = '';
 
-    await new Promise<void>(
-      (resolve) =>
-        (childProcess = exec(command, (e) => {
-          resolve();
-          if (killed) return;
-          if (e) fail++;
-          else succ++;
-        }))
-    );
+    const p = spawn(command[0]!, command.slice(1), {
+      shell: process.env.SHELL || true,
+    });
+
+    p.stdout?.setEncoding('utf8').on('data', (data) => {
+      combinedOutput += data;
+    });
+
+    p.stderr?.setEncoding('utf8').on('data', (data) => {
+      combinedOutput += data;
+    });
+
+    const { code } = await new Promise<{ code: number }>((resolve) => {
+      p.on('exit', (code) => {
+        resolve({ code: code || 0 });
+      });
+    });
+
+    if (code === 0) {
+      succ++;
+    } else {
+      fail++;
+      if (combinedOutput) {
+        errors.add(combinedOutput);
+      }
+    }
 
     if (killed) {
       break;
